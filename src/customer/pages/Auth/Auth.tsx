242906f43fdda9e8368d5c84c6f56373c3abb3a9
RockingChauhan <sudhanshuchauhanrocks@gmail.com>
2025-01-13 14:26:36 +0530

    Commit

--- src/customer/pages/Auth/Auth.tsx
+++ src/customer/pages/Auth/Auth.tsx
@@ -1,39 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import LoginForm from './LoginForm'
+import React, { useEffect, useState } from 'react';
+import LoginForm from './LoginForm';
 import { Alert, Button, Snackbar } from '@mui/material';
 import SignupForm from './SignupForm';
 import { useAppSelector } from '../../../Redux Toolkit/Store';
 
 const Auth = () => {
     const [isLoginPage, setIsLoginPage] = useState(true);
-    const handleCloseSnackbar = () => setSnackbarOpen(false)
-    const { auth } = useAppSelector(store => store)
+    const handleCloseSnackbar = () => setSnackbarOpen(false);
+    const { auth } = useAppSelector(store => store);
     const [snackbarOpen, setSnackbarOpen] = useState(false);
 
     useEffect(() => {
-
         if (auth.otpSent || auth.error) {
             setSnackbarOpen(true);
-            console.log("store ", auth.error)
+            console.log("store ", auth.error);
         }
-
-    }, [auth.otpSent,auth.error])
+    }, [auth.otpSent, auth.error]);
 
     return (
         <div className='flex justify-center h-[90vh] items-center'>
-            <div className='max-w-md h-[85vh] rounded-md border shadow-lg '>
-                <img className='w-full rounded-t-md' src="/turquoise-background-designify.png" alt="" />
-                <div className='mt-8 px-10'>
+            <div className='max-w-md w-full h-[85vh] rounded-md border shadow-lg'>
+                <img className='w-full rounded-t-md' src="/turquoise-background-designify.png" alt="Background" />
+                <div className='mt-8 px-6 md:px-10'>
                     {isLoginPage ? <LoginForm /> : <SignupForm />}
 
                     <div className='flex items-center gap-1 justify-center mt-5'>
-                        <p>{isLoginPage && "Don't"} have Account ?</p>
-                        <Button onClick={() => setIsLoginPage(!isLoginPage)} size='small'>{isLoginPage ? "create account" : "login"}</Button>
+                        <p>{isLoginPage && "Don't"} have an account?</p>
+                        <Button onClick={() => setIsLoginPage(!isLoginPage)} size='small'>
+                            {isLoginPage ? "Create account" : "Login"}
+                        </Button>
                     </div>
                 </div>
-
-
             </div>
+
             <Snackbar
                 anchorOrigin={{ vertical: "top", horizontal: "right" }}
                 open={snackbarOpen} autoHideDuration={6000}
@@ -41,15 +40,15 @@ const Auth = () => {
             >
                 <Alert
                     onClose={handleCloseSnackbar}
-                    severity={auth.error?"error":"success"}
+                    severity={auth.error ? "error" : "success"}
                     variant="filled"
                     sx={{ width: '100%' }}
                 >
-                    {auth.error?auth.error : " otp sent to your email!"}
+                    {auth.error ? auth.error : "OTP sent to your email!"}
                 </Alert>
             </Snackbar>
         </div>
-    )
+    );
 }
 
-export default Auth
+export default Auth;
